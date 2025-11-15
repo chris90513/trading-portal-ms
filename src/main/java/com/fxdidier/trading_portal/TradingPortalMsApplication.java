8758e89e03f2bff7f02e5e7783cb8dab0fbad07b
Christian Didier Almaraz <chris905131@gmail.com>
2025-11-15 14:42:19 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/fxdidier/trading_portal/TradingPortalMsApplication.java
@@ -0,0 +1,13 @@
+package com.fxdidier.trading_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPortalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingPortalMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fxdidier/trading_portal/TradingPortalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxdidier.trading_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingPortalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
